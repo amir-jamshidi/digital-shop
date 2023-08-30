237b01c447c976b96e46e17ae390eb13188cba33
Amir Hossein Jamshidi <devAmir.ir@gmail.com>
2023-08-30 12:39:44 +0330

    style : Fix Cart Item And SideBar Item Style Border

--- src/Components/SideBar/SidebarItem.jsx
+++ src/Components/SideBar/SidebarItem.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const SidebarItem = ({ productId, productImg, productName, productPrice }) => {
   return (
     <>
+      <hr className="hr-line-sidebar" />
       <div className="sidebar-item">
         <img src={productImg[0]} alt="" className="col-3 item-img" />
         <div className="col-9">
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import MainContext from "./../Contexts/MainContext";
 import { RemoveCircleRounded } from "@mui/icons-material";
 import Separator from "./../Utils/Separator";
+import { Link } from "react-router-dom";
 const Cart = () => {
   const { cart, removeItemCart } = useContext(MainContext);
   let totalPrice = 0;
@@ -18,52 +19,60 @@ const Cart = () => {
                   {cart.map((c, index) => {
                     totalPrice += c.productPrice * c.productCount;
                     return (
-                      <li key={c.id}>
-                        <div className="row w-100">
-                          <span className="d-none d-lg-block col-lg-1 mt-lg-0 align-self-center cart-item-count">
-                            {index + 1}
-                          </span>
+                      <>
+                        <li key={c.id} className="cart-item">
+                          <div className="row w-100">
+                            <span className="d-none d-lg-block col-lg-1 mt-lg-0 align-self-center cart-item-count">
+                              {index + 1}
+                            </span>
 
-                          <div className="col-12 col-lg-2 mt-lg-0 cart-item-img-parent">
-                            <img
-                              className=""
-                              width={100}
-                              src={c.productImg}
-                              alt=""
-                            />
+                            <div className="col-12 col-lg-2 mt-lg-0 cart-item-img-parent">
+                              <img
+                                className=""
+                                width={100}
+                                src={c.productImg}
+                                alt=""
+                              />
+                            </div>
+
+                            <span className="col-12 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-title ">
+                              <Link
+                                className="cart-item-title-link"
+                                to={`/product/${c.productId}`}
+                              >
+                                {c.productName}
+                              </Link>
+                            </span>
+
+                            <div className="col-12 mt-2 col-lg-1 mt-lg-0  cart-item-color-parent">
+                              <span
+                                className="cart-item-color"
+                                style={{ backgroundColor: c.productColors }}
+                              ></span>
+                            </div>
+
+                            <span className="col-5 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-price">
+                              {Separator(c.productPrice)}
+                            </span>
+
+                            <span className="col-2 mt-3 col-lg-1 mt-lg-0 align-self-center cart-item-count">
+                              1
+                            </span>
+
+                            <span className="col-5 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-amount">
+                              {Separator(c.productPrice * c.productCount)}
+                            </span>
+
+                            <div
+                              onClick={() => removeItemCart(c.id)}
+                              className="col-12 mt-3 col-lg-1 mt-lg-0 cart-item-icon-parent"
+                            >
+                              <RemoveCircleRounded className="remove-icon" />
+                            </div>
                           </div>
-
-                          <span className="col-12 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-title ">
-                            {c.productName}
-                          </span>
-
-                          <div className="col-12 mt-2 col-lg-1 mt-lg-0  cart-item-color-parent">
-                            <span
-                              className="cart-item-color"
-                              style={{ backgroundColor: c.productColors }}
-                            ></span>
-                          </div>
-
-                          <span className="col-5 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-price">
-                            {Separator(c.productPrice)}
-                          </span>
-
-                          <span className="col-2 mt-3 col-lg-1 mt-lg-0 align-self-center cart-item-count">
-                            1
-                          </span>
-
-                          <span className="col-5 mt-3 col-lg-2 mt-lg-0 align-self-center cart-item-amount">
-                            {Separator(c.productPrice * c.productCount)}
-                          </span>
-
-                          <div
-                            onClick={() => removeItemCart(c.id)}
-                            className="col-12 mt-3 col-lg-1 mt-lg-0 cart-item-icon-parent"
-                          >
-                            <RemoveCircleRounded className="remove-icon" />
-                          </div>
-                        </div>
-                      </li>
+                        </li>
+                        <hr className="hr-line" />
+                      </>
                     );
                   })}
                 </ul>
