d32cdf397c7ccf89a1d92c09e50473e70ea0f8a5
Amir Hossein Jamshidi <devAmir.ir@gmail.com>
2023-08-31 21:03:30 +0330

    style : Fix Drawer Styles - DrawerRight.jsx

--- src/Components/Drawer/DrawerRight.jsx
+++ src/Components/Drawer/DrawerRight.jsx
@@ -3,6 +3,11 @@ import { useContext } from "react";
 import MainContext from "../../Contexts/MainContext";
 import { CloseTwoTone } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import {
+  HomeRounded,
+  InfoRounded,
+  AccountCircleRounded,
+} from "@mui/icons-material";
 const DrawerRight = () => {
   const { drawer, manageDrawer } = useContext(MainContext);
 
@@ -16,13 +21,38 @@ const DrawerRight = () => {
         </div>
 
         <div className="drawer-nav">
-            <ul className="drawer-list">
-            <li onClick={()=>manageDrawer(false)}><Link to='/'>صفحه اصلی</Link> </li>
-            <li onClick={()=>manageDrawer(false)}><Link to='/about'>درباره ما</Link> </li>
-            <li onClick={()=>manageDrawer(false)}><Link to='/contact'>تماس با ما</Link> </li>
-             
-            </ul>
-          </div>
+          <ul className="drawer-list">
+            <li
+              className="d-flex justify-content-center align-items-center"
+              onClick={() => manageDrawer(false)}
+            >
+              <span className="px-2">
+                <HomeRounded />
+              </span>
+              <Link to="/">صفحه اصلی</Link>{" "}
+            </li>
+            <hr className="hr-line-sidebar" />
+            <li
+              className="d-flex justify-content-center align-items-center"
+              onClick={() => manageDrawer(false)}
+            >
+              <span className="px-2">
+                <InfoRounded />
+              </span>
+              <Link to="/about">درباره ما</Link>{" "}
+            </li>
+            <hr className="hr-line-sidebar" />
+            <li
+              className="d-flex justify-content-center align-items-center"
+              onClick={() => manageDrawer(false)}
+            >
+              <span className="px-2">
+                <AccountCircleRounded />
+              </span>
+              <Link to="/contact">تماس با ما</Link>{" "}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
